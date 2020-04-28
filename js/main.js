58075ecf80caf94bc098f9407247dcace54b2c7c
namuj <namuj_ssa@hotmail.com>
2020-04-28 09:10:20 -0300

    definindo Game over!

--- js/main.js
+++ js/main.js
@@ -73,11 +73,20 @@ function atualizar (event){
 }
 
 function iniciarJogo(){
+	
+	
+	
 	if(cobra[0].x >15*box && direcao=="right") cobra[0].x=0;
 	if(cobra[0].x <0 && direcao=="left") cobra[0].x=16*box;
 	if(cobra[0].y >15*box && direcao=="down") cobra[0].y=0;
 	if(cobra[0].y <0 && direcao=="up") cobra[0].y=16*box;
 	
+	for(i=1;i<cobra.length;i++){
+		if(cobra[0].x==cobra[i].x && cobra[0].y==cobra[i].y){
+			clearInterval(jogo);
+			alert("Game Over =/ ");
+		}
+	}
 	
 	criarField();
 	criarCobrinha();
