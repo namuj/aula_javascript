9ef73cf0ec7146b9074612c48f4ae40b39edc389
namuj <namuj_ssa@hotmail.com>
2020-04-28 08:07:18 -0300

    criar cobrinha

--- js/main.js
+++ js/main.js
@@ -30,12 +30,25 @@ numero--;
 let canvas = document.getElementById("cobra");
 let context = canvas.getContext("2d");
 let box = 32;
-
+let cobra =[];
+cobra[0]={
+	x:8*box,
+	y:8*box
+}
 function criarField(){
 	context.fillStyle= "lightgreen";
 	context.fillRect(0,0,16 * box, 16 * box);
 	
 	
 }
+function criarCobrinha(){
+	for(i=0; i< cobra.length; i++){
+		context.fillStyle="green";
+		context.fillRect(cobra[i].x,cobra[i].y,box,box);
+	}
+	
+	
+}
 
 criarField();
+criarCobrinha();
