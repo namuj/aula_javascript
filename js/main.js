77fcb6179126ff5e5b81680ad958855bb1424151
namuj <namuj_ssa@hotmail.com>
2020-04-28 08:37:55 -0300

    atravessar as margens do field

--- js/main.js
+++ js/main.js
@@ -63,6 +63,12 @@ function atualizar (event){
 }
 
 function iniciarJogo(){
+	if(cobra[0].x >15*box && direcao=="right") cobra[0].x=0;
+	if(cobra[0].x <0 && direcao=="left") cobra[0].x=16*box;
+	if(cobra[0].y >15*box && direcao=="down") cobra[0].y=0;
+	if(cobra[0].y <0 && direcao=="up") cobra[0].y=16*box;
+	
+	
 	criarField();
 	criarCobrinha();
 	
