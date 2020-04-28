055c4c3a674a55159d62773c0dbedbfbabbd8ae6
namuj <namuj_ssa@hotmail.com>
2020-04-28 07:58:20 -0300

    criação do background

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+/*var nome ="Namuj";
+
+var idade = 32;
+var idade2 = 2;
+var d= new Date();
+var lista = ["maçã","limão","uva"];
+var animal ={nome:"Leão", patas:3,especie:"felino"};
+
+function divisao(n1,n2){
+	
+	return n1/n2;
+	
+}
+
+alert(divisao(5,5));
+
+var numero= prompt("Escolha um número:");
+	alert(d);
+
+do{
+if(numero==1){
+	alert("acertou");
+}else{
+		alert("errou");
+};
+numero--;
+}while(numero!=0);*/
+
+let canvas = document.getElementById("cobra");
+let context = canvas.getContext("2d");
+let box = 32;
+
+function criarField(){
+	context.fillStyle= "lightgreen";
+	context.fillRect(0,0,16 * box, 16 * box);
+	
+	
+}
+
+criarField();
